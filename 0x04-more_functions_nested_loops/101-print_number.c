cf0a6b1ada0652e86f087198ed44416cd476b6b8
alaatarek1 <alaatarek928@yahoo.com>
2023-03-29 17:13:21 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,48 @@
-include"main.h"
-
+#include "main.h"
 /**
- * print_number - Prints an integer.
- *
- * @n: The integer to be printed.
-*/
-
+ * print_number - print diagonal line
+ * @n: number of time back slash is printed
+ */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int size = n;
 
-	/*first check if its negative*/
+	int digits = 0;
+
+	int i;
+
 	if (n < 0)
+		size = -n;
+	if (n == 0)
+		digits = 1;
+
+	while (size >= 1)
 	{
-		_putchar('-');
-		num = -num;
+		size = size / 10;
+		digits++;
 	}
 
-	/*print the first few digits*/
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	for (i = 0; i < digits; i++)
+	{
 
-	/*print the last digit*/
-	_putchar((num % 10) + 48);
+		int pow = 1;
+
+		int j;
+
+		int d;
+
+		for (j = 0; j < digits - i - 1; j++)
+		{
+			pow = pow * 10;
+		}
+
+		d = ((n / pow) % 10);
+		if (n < 0)
+		{
+			d = -d;
+			if (i == 0)
+				_putchar(45);
+		}
+		_putchar(48 + d);
+	}
 }
