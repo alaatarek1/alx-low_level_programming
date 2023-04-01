115965da088dae91ee675d01c4a8a5ce60a59aaf
alaatarek1 <alaatarek928@yahoo.com>
2023-04-01 11:00:47 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - checks for alpha letters
+ * Return: the last digit
+ * @n: int to be checked
+ */
+int print_last_digit(int n)
+{
+	int d = n % 10;
+
+	if (n < 0)
+		d = -d;
+	_putchar(48 + d);
+	return (d);
+}
